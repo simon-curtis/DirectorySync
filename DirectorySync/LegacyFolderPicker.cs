1a686a12bb61b52efac60648a08b44c1de3dd97b
Simon Curtis <simon.curtis93@gmail.com>
2020-12-07 15:17:54 +0000

    Moved LegacyFolderPicker as this is implementation

---
+++ DirectorySync/LegacyFolderPicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace DirectorySync
+{
+    public static class LegacyFolderPicker
+    {
+        public static string GetFolder(string title)
+        {
+            var openFileDialog = new FolderBrowserDialog
+            {
+                Description = title
+            };
+            return openFileDialog.ShowDialog() == DialogResult.OK 
+                ? openFileDialog.SelectedPath 
+                : "";
+        }
+    }
+}
