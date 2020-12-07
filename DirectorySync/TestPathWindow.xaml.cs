92b2a32b4009c2c2012014223a53818b3586c829
Simon Curtis <simon.curtis93@gmail.com>
2020-12-07 15:13:19 +0000

    Created Test function to check ignores file is working correctly

---
+++ DirectorySync/TestPathWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows;
+using FileCompare;
+
+namespace DirectorySync
+{
+    public partial class TestPathWindow : Window
+    {
+        private string FilterPath { get; set; }
+
+        public TestPathWindow(string filterPath)
+        {
+            FilterPath = filterPath;
+            InitializeComponent();
+        }
+
+        private void RunTest_OnClick(object sender, RoutedEventArgs e)
+        {
+            var matches = FileFinderService.TestFile(FilterPath, PathToTest.Text);
+            foreach (var match in matches)
+            {
+                Results.Text += match + "\r";
+            }
+        }
+    }
+}
--- FileCompare/FileFinderService.cs
+++ FileCompare/FileFinderService.cs
@@ -92,5 +92,40 @@ namespace SarmsMoveTo47
                 yield return file;
             }
         }
+
+        public static IEnumerable<string> TestFile(string filterPath, string relativeFilePath)
+        {
+            var directoryFiltersStrings = new List<string>();
+            var fileFiltersStrings = new List<string>();
+
+            foreach (var line in File.ReadAllLines(filterPath))
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+
+                var linePart = line[2..].Trim();
+
+                switch (line[0])
+                {
+                    case 'd':
+                        directoryFiltersStrings.Add($@"({linePart}$)");
+                        break;
+
+                    case 'f':
+                        fileFiltersStrings.Add($@"({linePart}$)");
+                        break;
+                }
+            }
+
+            var directoryFilters = new Regex(string.Join('|', directoryFiltersStrings), RegexOptions.IgnoreCase);
+            var fileFilters = new Regex(string.Join('|', fileFiltersStrings), RegexOptions.IgnoreCase);
+
+            var directory = string.Join("\\", relativeFilePath.Split('\\')[..^1]);
+            var directoryExlusions = directoryFilters.Matches(directory).Select(match => $"{match.Name} {match.Value}");
+
+            var filename = relativeFilePath.Split('\\')[^1];
+            var fileExclusions = fileFilters.Matches(filename).Select(match => $"{match.Name} {match.Value}");
+
+            return directoryExlusions.Concat(fileExclusions).ToArray();
+        }
     }
 }
